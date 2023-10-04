fdd00522229db1901947526f745903c9ba17410b
sxlwrl <Vadik120404@>
2023-10-04 15:19:49 +0300

    [remove] Remove files

--- src/lab1/routes/healthcheckRouter.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from "express";
-import {getHealthcheck} from "../controllers/healthcheckController";
-
-const router = Router();
-
-router.get('/healthcheck', getHealthcheck);
-
-export default router;
