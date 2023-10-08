3f5e4618474753ceb0b3d62c6cf82f9da850bbb6
sxlwrl <Vadik120404@>
2023-10-08 18:21:07 +0300

    [change] Added initRoutes method to initialize routes in the application. The method takes an array of routes and connects them to the application root path.
    Added 404 error handler

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import {json} from "body-parser";
 
 class App {
     public app: express.Application;
 
-    constructor() {
+    constructor(controllers: any[]) {
         this.app = express();
 
         this.initMiddlewares();
+        this.initRoutes(controllers);
+
+        this.app.use((req: Request, res: Response, next: NextFunction) => {
+            res.status(404).json({code: 404, status: 'Not Found'});
+            next();
+        });
     };
 
     public listen(): void {
@@ -18,7 +24,11 @@ class App {
 
     private initMiddlewares(): void {
         this.app.use(json());
-    }
+    };
+
+    private initRoutes(routes: any[]): void {
+        routes.forEach(routerItem => this.app.use('/', new routerItem().router));
+    };
 }
 
 export default App;
