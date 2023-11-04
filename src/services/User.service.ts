ec9a53209f7654772bdd7bb7007cc9537d222d35
sxlwrl <vadymkazak2019@gmail.com>
2023-11-04 14:05:53 +0200

    [change] added service functions for getting user by id, deleting and getting all users

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -1,10 +1,22 @@
-import {IUserRepository} from "../repositories/IUserRepository";
-import {CreateUserDto} from "../dto/User/CreateUser.dto";
+import { IUserRepository } from '../repositories/User/IUserRepository';
+import { CreateUserDto } from '../dto/CreateUser.dto';
 
 export class UserService {
-    constructor(private readonly _userRepository: IUserRepository) {};
+    constructor(private readonly _userRepository: IUserRepository) {}
 
-    createUser(data: CreateUserDto) {
+    public getUserById(id: string) {
+        return this._userRepository.getUserById(id);
+    };
+
+    public deleteUser(id: string) {
+        return this._userRepository.deleteUser(id);
+    };
+
+    public createUser(data: CreateUserDto) {
         return this._userRepository.createUser(data);
+    };
+
+    public getUsers() {
+        return this._userRepository.getUsers();
     };
 }
