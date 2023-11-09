fc506f877d9be367a4912fa9f1fca8a54d28f0ba
sxlwrl <vadymkazak2019@gmail.com>
2023-11-09 19:37:58 +0200

    [add] record service

---
+++ src/services/Record.service.ts
@@ -0,0 +1,14 @@
+import { IRecordRepository } from '../repositories/interfaces/IRecord.repository';
+import { CreateRecordDto } from '../dto/CreateRecord.dto';
+import { Service } from './Service';
+import { Record } from '../entities/Record';
+
+export class RecordService extends Service<Record, CreateRecordDto> {
+    constructor(private readonly _recordRepository: IRecordRepository) {
+        super(_recordRepository);
+    }
+
+    public getRecordFiltered(user_id: string, category_id: string) {
+        return this._recordRepository.getFiltered(user_id, category_id);
+    }
+}
