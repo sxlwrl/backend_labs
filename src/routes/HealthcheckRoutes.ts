b2e0bd106dfc320197dfc7c4e463ce8dfbae61c6
sxlwrl <vadymkazak2019@gmail.com>
2023-11-18 19:32:26 +0200

    [add] healthcheck router

---
+++ src/routes/HealthcheckRoutes.ts
@@ -0,0 +1,18 @@
+import {Router} from 'express';
+
+import {getHealthcheck} from "../controllers/HealthcheckController";
+import IRoutes from './routes-interfaces/IRoutes';
+
+class HealthcheckRoutes implements IRoutes {
+    public router: Router = Router();
+
+    constructor() {
+        this.initializeRoutes();
+    };
+
+    initializeRoutes() {
+        this.router.get('/healthcheck', getHealthcheck);
+    };
+}
+
+export default HealthcheckRoutes;
