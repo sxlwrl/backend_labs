4f715129fb8e3310b9e7401d1232570ff3a93500
sxlwrl <vadymkazak2019@gmail.com>
2023-11-18 19:39:47 +0200

    [change] added healthcheck router

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import App from './app';
 import userRoutes from './routes/UserRoutes';
 import categoryRoutes from './routes/CategoryRoutes';
 import recordRoutes from './routes/RecordRoutes';
+import HealthcheckRoutes from './routes/HealthcheckRoutes';
 
-const app = new App([new userRoutes(), new categoryRoutes(), new recordRoutes()]);
+const app = new App([new HealthcheckRoutes(), new userRoutes(), new categoryRoutes(), new recordRoutes()]);
 
 app.listen();
