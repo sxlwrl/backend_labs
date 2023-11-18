403cdcf124adfe36f7ac0c1a7fe6cc9ace3ffdfe
sxlwrl <vadymkazak2019@gmail.com>
2023-11-18 19:52:37 +0200

    [add] added Record Repository implementation

---
+++ src/repositories/RecordRepositoryImpl.ts
@@ -0,0 +1,18 @@
+import { IRecordRepository } from './interfaces/IRecordRepository';
+import { Record } from '../entities/Record';
+
+import { RepositoryImpl } from './RepositoryImpl';
+
+export class RecordRepositoryImpl extends RepositoryImpl<Record> implements IRecordRepository {
+    getFiltered(userId: string, categoryId: string) {
+        if (!categoryId) {
+            return this._data.filter(record => record.userId === userId);
+        }
+        else if (!userId) {
+            return this._data.filter(record => record.categoryId === categoryId);
+        }
+        else {
+            return this._data.filter(record => record.userId === userId && record.categoryId === categoryId);
+        }
+    }
+}
