114a2b0cd4ac1073d71847cba1926aec08bf86cd
sxlwrl <vadymkazak2019@gmail.com>
2023-11-18 19:11:39 +0200

    [removed] removed

--- src/repositories/User/IUserRepository.ts
+++
@@ -1,9 +0,0 @@
-import {User} from "../../entities/User";
-import {CreateUserDto} from "../../dto/CreateUser.dto";
-
-export interface IUserRepository {
-    getUserById(id: string): User | undefined;
-    deleteUser(id: string): void;
-    createUser(dto: CreateUserDto): User;
-    getUsers(): Array<User>;
-}
