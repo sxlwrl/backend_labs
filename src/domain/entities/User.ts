91f8ef3d01690bf439ee5d0886c2d35218d1a46d
sxlwrl <vadymkazak2019@gmail.com>
2023-12-22 22:32:11 +0200

    [change] Added getter to get default currency

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,7 +1,11 @@
 export class User {
-    constructor(private _username: string) {};
+    constructor(private _username: string, private _defaultCurrency?: string | undefined) {};
 
     get getUsername(): string {
         return this._username;
+    };
+
+    get getDefaultCurrency(): string | undefined {
+        return this._defaultCurrency;
     };
 }
