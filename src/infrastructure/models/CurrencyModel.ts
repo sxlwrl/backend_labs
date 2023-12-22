da5b57590ad2e38a6024818fd623f0f81e36351f
sxlwrl <vadymkazak2019@gmail.com>
2023-12-22 21:41:01 +0200

    [add] Currency Model

---
+++ src/infrastructure/models/CurrencyModel.ts
@@ -0,0 +1,35 @@
+import {DataTypes, Optional, Model} from 'sequelize';
+import {sequelize} from "../database/sequelize";
+
+interface CurrencyAttributes {
+    id: number;
+    currencyName: string;
+}
+
+interface CurrencyCreationAttributes extends Optional<CurrencyAttributes, 'id'> {
+}
+
+export class CurrencyModel extends Model<CurrencyAttributes, CurrencyCreationAttributes> {
+    declare id: number;
+    declare currencyName: string;
+}
+
+CurrencyModel.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    currencyName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'UAH'
+    }
+}, {
+    sequelize,
+    tableName: 'currencies',
+    modelName: 'currency'
+});
+
+
