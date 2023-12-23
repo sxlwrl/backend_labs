fcfa4388d4877a0a634b7bfd0830e7889da4126d
sxlwrl <vadymkazak2019@gmail.com>
2023-12-23 16:23:46 +0200

    [add] Replaced record repository interface. Changed function type

---
+++ src/domain/repositories/interfaces/IRecordRepository.ts
@@ -0,0 +1,7 @@
+import {Record} from "../../entities/Record";
+import {RecordModel} from "../../../infrastructure/models/RecordModel";
+import {IRepository} from "./IRepository";
+
+export interface IRecordRepository extends IRepository<Record, RecordModel> {
+    getFiltered(user_id: number | undefined, category_id: number | undefined): Promise<Array<RecordModel>>;
+}
