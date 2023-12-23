cbfd9c57f385de684a4cd7a1ac6771c98bb90ecb
sxlwrl <vadymkazak2019@gmail.com>
2023-12-23 14:39:40 +0200

    [move] Files has been moved

--- src/controllers/HealthcheckController.ts
+++
@@ -1,5 +0,0 @@
-import {RequestHandler} from "express";
-
-export const getHealthcheck: RequestHandler = (req, res, next) => {
-    res.status(200).json({date: new Date(), status: 'OK'});
-};
--- src/repositories/interfaces/ICategoryRepository.ts
+++
@@ -1,5 +0,0 @@
-import { Category } from '../../entities/Category';
-import { CreateCategoryDto } from '../../dto/CreateCategory.dto';
-import { IRepository } from './IRepository';
-
-export interface ICategoryRepository extends IRepository<Category, CreateCategoryDto> {}
