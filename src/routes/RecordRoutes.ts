cf2dbb080a9a23b54694cd000e10d74420523a3d
sxlwrl <vadymkazak2019@gmail.com>
2023-12-23 16:21:19 +0200

    [change] Added validation

--- src/routes/RecordRoutes.ts
+++ src/routes/RecordRoutes.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express';
+import {body} from 'express-validator';
 
-import {RecordRepositoryImpl} from '../repositories/RecordRepositoryImpl';
-import {RecordController} from '../controllers/RecordController';
-import {RecordService} from '../services/RecordService';
+import {RecordRepositoryImpl} from "../domain/repositories/RecordRepositoryImpl";
+import {RecordController} from "../infrastructure/controllers/RecordController";
+import {RecordService} from '../domain/services/RecordService';
 
 import IRoutes from './routes-interfaces/IRoutes';
+import {checkValidation} from "../middleware/checkValidation.middleware";
 
 class UserRoutes implements IRoutes {
     public router: Router = Router();
@@ -18,7 +20,9 @@ class UserRoutes implements IRoutes {
     initializeRoutes() {
         this.router.get('/record/:record_id', this.recordController.getRecordById);
         this.router.delete('/record/:record_id', this.recordController.deleteRecord);
-        this.router.post('/record', this.recordController.createRecord);
+        this.router.post('/record', [body(['userId', 'categoryId']).custom(value => {
+            return typeof value === 'number' && value >= 1;
+        })], checkValidation ,this.recordController.createRecord);
         this.router.get('/record', this.recordController.getRecordFiltered);
     };
 }
