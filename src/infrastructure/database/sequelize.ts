9a4f6928c7ab25aa3873cc5822328e0abd1bbc91
sxlwrl <vadymkazak2019@gmail.com>
2023-12-23 17:18:27 +0200

    [change] Path change

--- src/infrastructure/database/sequelize.ts
+++ src/infrastructure/database/sequelize.ts
@@ -1,4 +1,4 @@
 import { Sequelize } from 'sequelize-typescript';
-import { config } from '../../../config/config';
+import { config } from '../../../config/config.ts';
 
 export const sequelize = new Sequelize(config);
