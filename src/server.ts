66d5d4c6e8a7f3b25074762c1a6ebf0827c9137b
sxlwrl <vadymkazak2019@gmail.com>
2023-12-23 17:58:07 +0200

    [change] replacement server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import App from './app';
+import App from './application/app.ts';
 
-import HealthcheckRoutes from '../routes/HealthcheckRoutes';
-import userRoutes from '../routes/UserRoutes';
-import categoryRoutes from "../routes/CategoryRoutes";
-import recordRoutes from "../routes/RecordRoutes";
+import HealthcheckRoutes from './routes/HealthcheckRoutes.ts';
+import userRoutes from './routes/UserRoutes.ts';
+import categoryRoutes from "./routes/CategoryRoutes.ts";
+import recordRoutes from "./routes/RecordRoutes.ts";
 
-import {sequelize} from "../infrastructure/database/sequelize";
-import {CurrencyModel} from "../infrastructure/models/CurrencyModel";
+import {sequelize} from "./infrastructure/database/sequelize.ts";
+import {CurrencyModel} from "./infrastructure/models/CurrencyModel.ts";
 
 const app = new App([new HealthcheckRoutes(), new userRoutes(), new categoryRoutes(), new recordRoutes()]);
 
