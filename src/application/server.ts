1a5c3e0738b681452616609e43e5dd470a772dea
sxlwrl <vadymkazak2019@gmail.com>
2023-12-23 17:21:06 +0200

    [change] Server changes
    - Add logic to populate the currencies table with default values if empty on server start
    
    - Set up the database connection using Sequelize.

--- src/application/server.ts
+++ src/application/server.ts
@@ -1,8 +1,37 @@
 import App from './app';
-import userRoutes from '../routes/UserRoutes';
 
 import HealthcheckRoutes from '../routes/HealthcheckRoutes';
+import userRoutes from '../routes/UserRoutes';
+import categoryRoutes from "../routes/CategoryRoutes";
+import recordRoutes from "../routes/RecordRoutes";
 
-const app = new App([new HealthcheckRoutes(), new userRoutes()]);
+import {sequelize} from "../infrastructure/database/sequelize";
+import {CurrencyModel} from "../infrastructure/models/CurrencyModel";
 
-app.listen();
+const app = new App([new HealthcheckRoutes(), new userRoutes(), new categoryRoutes(), new recordRoutes()]);
+
+async function startServer() {
+    try {
+        // await sequelize.sync({ force: true });
+        // await sequelize.sync();
+        if (await CurrencyModel.count() === 0) {
+            const currencies = [
+                {currencyName: 'UAH'},
+                {currencyName: 'EUR'},
+                {currencyName: 'USD'},
+                {currencyName: 'GBP'}
+            ];
+
+            await CurrencyModel.bulkCreate(currencies);
+        }
+
+        app.listen();
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            throw new Error(err.message);
+        }
+    }
+}
+
+(async () => await startServer())();
+
