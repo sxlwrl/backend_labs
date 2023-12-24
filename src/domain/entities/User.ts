7b29a807576d7d3adec178c1999ee18ee69ce6e9
sxlwrl <vadymkazak2019@gmail.com>
2023-12-24 16:43:39 +0200

    [change] added field with password
    - added getter for getting password

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,8 +1,16 @@
 export class User {
-    constructor(private _username: string, private _defaultCurrency?: string | undefined) {};
+    constructor(
+        private _username: string,
+        private _password: string,
+        private _defaultCurrency?: string | undefined
+    ) {};
 
     get getUsername(): string {
         return this._username;
+    };
+
+    get getPassword(): string {
+        return this._password;
     };
 
     get getDefaultCurrency(): string | undefined {
